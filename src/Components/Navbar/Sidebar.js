b8b5b20ba300b62fa3161d2305f0c3d3d98c2f4b
Dev-Sivasundari <sivasundarismrft@gmail.com>
2026-02-10 11:23:14 +0530

    Update Sidebar.js

--- src/Components/Navbar/Sidebar.js
+++ src/Components/Navbar/Sidebar.js
@@ -1286,12 +1286,6 @@ const Sidebar = () => {
                 >
                   B2B Approval
                 </SubLink>
-                <SubLink
-                  to="/B2BPackage"
-                  onClick={() => setIsSidebarOpen(false)}
-                >
-                  B2B Package
-                </SubLink>
               </DropdownContent>
 
               <SectionDivider />
@@ -1329,9 +1323,6 @@ const Sidebar = () => {
               </DropdownHeader>
 
               <DropdownContent isOpen={dropdowns.billingDetails}>
-                <SubLink to="/PatientBilling" onClick={() => setIsSidebarOpen(false)}>
-                  Diagnostics Billing
-                </SubLink>
                 <SubLink to="/PrintBill" onClick={() => setIsSidebarOpen(false)}>
                   Patient Overview
                 </SubLink>
@@ -1380,13 +1371,13 @@ const Sidebar = () => {
               </DropdownHeader>
 
               <DropdownContent isOpen={dropdowns.logisticsDetails}>
-                <SubLink to="/LogisticManagementAdmin" onClick={() => setIsSidebarOpen(false)}>
+                <SubLink to="/LogisticsTaskAssign" onClick={() => setIsSidebarOpen(false)}>
                   Logistics Task Assigning
                 </SubLink>
                 <SubLink to="/LogisticsDashboard" onClick={() => setIsSidebarOpen(false)}>
                   Logistics Dashboard
                 </SubLink>
-                <SubLink to="/LogisticMap" onClick={() => setIsSidebarOpen(false)}>
+                <SubLink to="/LogisticsTracking" onClick={() => setIsSidebarOpen(false)}>
                   Logistics Tracking
                 </SubLink>
               </DropdownContent>
@@ -1612,3 +1603,4 @@ const Sidebar = () => {
 
 
 export default Sidebar
+
