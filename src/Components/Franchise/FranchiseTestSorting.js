6ef0895df7ebf65af23aef5822bb6fd57cad3b87
dev/najma <najmasmrft@gmail.com>
2026-05-13 13:00:11 +0530

    logo image change

--- src/Components/Franchise/FranchiseTestSorting.js
+++ src/Components/Franchise/FranchiseTestSorting.js
@@ -853,7 +853,7 @@ const FranchiseTestSorting = ({ patient, onClose }) => {
             const nablLogoWidth = 25;
             const nablLogoHeight = 25;
             const nablLogoX = doc.internal.pageSize.width - 40 - nablLogoWidth;
-            const nablLogoY = 9;
+            const nablLogoY = 8;
             doc.addImage(
               NABLImage,
               "PNG",
--- src/Components/Report/PatientOverview.js
+++ src/Components/Report/PatientOverview.js
@@ -1283,7 +1283,7 @@ const PatientOverview = () => {
             const nablLogoWidth = 25;
             const nablLogoHeight = 25;
             const nablLogoX = doc.internal.pageSize.width - 40 - nablLogoWidth;
-            const nablLogoY = 9;
+            const nablLogoY = 8;
             doc.addImage(
               NABLImage,
               "PNG",
